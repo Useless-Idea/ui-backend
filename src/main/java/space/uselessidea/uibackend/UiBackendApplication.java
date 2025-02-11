eb624f92f101e797db7ec2ed6714081d69291094
Pawel Sikora <sikoreq@gmail.com>
2025-02-11 21:53:26 +0100

    add checkstyle and lombok

---
+++ src/main/java/space/uselessidea/uibackend/UiBackendApplication.java
@@ -0,0 +1,14 @@
+package space.uselessidea.uibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UiBackendApplication.class, args);
+  }
+
+
+}
---
+++ src/test/java/space/uselessidea/uibackend/UiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package space.uselessidea.uibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
