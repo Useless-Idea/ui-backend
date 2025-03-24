0b39b1e10fbaf8cb430de5b577528042048cb514
Pawel Sikora <sikoreq@gmail.com>
2025-03-24 12:23:13 +0100

    auto unlock uselsess corp

--- src/main/resources/db/changelog/002-inserts.sql
+++ src/main/resources/db/changelog/002-inserts.sql
@@ -10,5 +10,15 @@ VALUES ('ShowChar', 'SHOW_CHAR', 'Show List of all char');
 INSERT INTO permission_role (role_uuid, permission_uuid)
 VALUES ((select r."uuid" from "role" r where r.code = 'ADMIN'),
         (select p."uuid" from "permission" p where p.code = 'SHOW_CHAR'));
+--changeset psikora:2
+INSERT INTO corporation
+    (id, "name", is_blocked, alliance_id)
+VALUES (98067874, 'Useless Idea', false, NULL)
+    ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.corporation
+    (id, "name", is_blocked, alliance_id)
+VALUES (98796489, 'Not So Useless', false, NULL)
+    ON CONFLICT (id) DO NOTHING;
 
 
