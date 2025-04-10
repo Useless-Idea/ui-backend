9ea5e4187bab132638dda8596e1707bfb3fa5272
Pawel Sikora <sikoreq@gmail.com>
2025-04-10 18:17:10 +0200

    enable token refresh

--- src/main/java/space/uselessidea/uibackend/infrastructure/eve/api/token/TokenRefreshCronJob.java
+++ src/main/java/space/uselessidea/uibackend/infrastructure/eve/api/token/TokenRefreshCronJob.java
@@ -3,6 +3,7 @@ package space.uselessidea.uibackend.infrastructure.eve.api.token;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import space.uselessidea.uibackend.domain.token.port.primary.TokenPrimaryPort;
 
@@ -15,7 +16,7 @@ public class TokenRefreshCronJob {
 
 
   @Transactional
-  //@Scheduled(cron = "${cronjob.tokenrefresh}")
+  @Scheduled(cron = "${cronjob.tokenrefresh}")
   //@Scheduled(fixedDelay = 10 * 60 * 1000)
   public void refreshAllTokens() {
     log.info("refreshAllTokens---START");
