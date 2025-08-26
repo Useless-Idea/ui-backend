ac4ac6041681a90579e8765797122156334089bd
Pawel Sikora <sikoreq@gmail.com>
2025-08-26 10:49:46 +0200

    sp Reminder

---
+++ src/main/java/space/uselessidea/uibackend/infrastructure/cron/SpReminderCronjob.java
@@ -0,0 +1,12 @@
+package space.uselessidea.uibackend.infrastructure.cron;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpReminderCronjob {
+
+  public void reminder() {
+
+  }
+
+}
