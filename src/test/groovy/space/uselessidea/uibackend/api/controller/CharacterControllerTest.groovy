904529bb09c03ea7dfe8a75b8be6cb0a56eb1a2b
Pawel Sikora <sikoreq@gmail.com>
2026-03-15 11:03:10 +0100

    Dodanie usuwania fitow, poprawa testów

--- src/test/groovy/space/uselessidea/uibackend/api/controller/CharacterControllerTest.groovy
+++ src/test/groovy/space/uselessidea/uibackend/api/controller/CharacterControllerTest.groovy
@@ -12,7 +12,7 @@ import space.uselessidea.uibackend.domain.token.port.primary.TokenPrimaryPort
 
 import static io.restassured.RestAssured.given
 import static org.hamcrest.Matchers.equalTo
-import static org.mockito.ArgumentMatchers.any
+import static org.mockito.ArgumentMatchers.nullable
 import static org.mockito.Mockito.when
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -26,12 +26,11 @@ class CharacterControllerTest extends RestAssuredSpecification {
 
     def "should get user token"() {
         given:
-        when(characterApiService.getCharacterFeatures(2120732484L)).thenReturn([
+        when(characterApiService.getCharacterFeatures(nullable(Long.class))).thenReturn([
                 CharacterFeature.builder().feature(FeatureEnum.USER_STANDINGS).active(true).build(),
                 CharacterFeature.builder().feature(FeatureEnum.OPEN_CONTRACTS).active(false).build(),
                 CharacterFeature.builder().feature(FeatureEnum.USER_SKILL).active(true).build()
         ] as Set)
-        when(tokenPrimaryPort.getAccessToken(2120732484L)).thenReturn(Optional.of("eyJhbGciOiJSUzI1NiIsImtpZCI6IkpXVC1TaWduYXR1cmUtS2V5IiwidHlwIjoiSldUIn0.eyJzY3AiOlsiZXNpLXNraWxscy5yZWFkX3NraWxscy52MSIsImVzaS1jaGFyYWN0ZXJzLnJlYWRfc3RhbmRpbmdzLnYxIl0sImp0aSI6IjliMGUwYmMyLTVjM2MtNDRlZS05YWVlLWZlOTYyMGEzMzI4NyIsImtpZCI6IkpXVC1TaWduYXR1cmUtS2V5Iiwic3ViIjoiQ0hBUkFDVEVSOkVWRToyMTE2NDk2MzE1IiwiYXpwIjoiOTJmNDFkNjI1YTU2NGVkYTgzZjQ1MWE2ODNhZTg0N2EiLCJ0ZW5hbnQiOiJ0cmFucXVpbGl0eSIsInRpZXIiOiJsaXZlIiwicmVnaW9uIjoid29ybGQiLCJhdWQiOlsiOTJmNDFkNjI1YTU2NGVkYTgzZjQ1MWE2ODNhZTg0N2EiLCJFVkUgT25saW5lIl0sIm5hbWUiOiJQYXVsIFRpdCIsIm93bmVyIjoieGZKNjJtYTkyN3Vhb3drY0JLMDMrMGl4ZFdZPSIsImV4cCI6MTc2MzYyMjA2NCwiaWF0IjoxNzYzNjIwODY0LCJpc3MiOiJodHRwczovL2xvZ2luLmV2ZW9ubGluZS5jb20ifQ.MctNFGHdmavwTezchtRSvLw183p3QszGm-CvRZjNe-2T36aIaVOAykgt21FSQ2bgB4ICi3qL1tzpFZaXfXSBGH-HICQRSieDbb7wZt6HYyw8Tn6opkNXDvHa98MNKeDj3-PImgqNd3ed1wWb1xPOb-JSK-4VV1oCn0ryeyi9jwT8puRcnONWfJcLM4OMW_NiIVd7X4tw3doSluu5qQWSz7_wjkTO8E4JCOIL5RGZP0YW2FZWXsGl7GX63ZrngxvcLlvIBzgqS9sF5gQNzyspGKvujYePLPAuGqrOs7AXz1h3MHMKS6niog8LDwGsKTpPeRVYY77YCmhUjsIqf68Fhw"))
         when:
         def response = given()
                 .header("Authorization", "Bearer ${UI_ADMIN_TOKEN}")
@@ -48,8 +47,7 @@ class CharacterControllerTest extends RestAssuredSpecification {
 
     def "should return empty character map"() {
         given:
-        when(characterApiService.getCharacterIdNameMap(any(CharacterPrincipal.class))).thenReturn([:])
-
+        when(characterApiService.getCharacterIdNameMap(nullable(CharacterPrincipal.class))).thenReturn([:])
         when:
         def response = given()
                 .header("Authorization", "Bearer ${UI_ADMIN_TOKEN}")
@@ -64,11 +62,10 @@ class CharacterControllerTest extends RestAssuredSpecification {
 
     def "should return character map with entries"() {
         given:
-        when(characterApiService.getCharacterIdNameMap(any(CharacterPrincipal.class))).thenReturn([
+        when(characterApiService.getCharacterIdNameMap(nullable(CharacterPrincipal.class))).thenReturn([
                 (1L): "Alpha",
                 (2L): "Beta"
         ])
-
         when:
         def response = given()
                 .header("Authorization", "Bearer ${UI_ADMIN_TOKEN}")
